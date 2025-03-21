2823aa4a6db3b48c647b4515d87c2ec2bc4285a3
Sergey Osokin <sergey.osokin@netfire.eu>
2025-03-21 16:58:07 +0100

    refactor: start changing to using one controller for one thing - finish start page.

--- htdocs/public/index.php
+++ htdocs/public/index.php
@@ -1,11 +1,11 @@
 <?php
 
 use Core\Database;
+use Core\Router;
 
 define('BASE_PATH', dirname(__DIR__) . '/'); // htdocs directory
 
 require_once BASE_PATH . 'Core/utils.php';
-
 include_once base_path('debug/error_log.php');
 // include('debug.php'); 
 
@@ -26,11 +26,12 @@ spl_autoload_register(function ($class) {
 });
 
 // require_once base_path('Core/Database.php');
+// require_once base_path('Core/Router.php');
 require_once base_path('models/Document.php');
 require_once base_path('models/User.php');
 
 // Initialize database connection
-$config = require base_path('Core/config.php');
+$config = require base_path('config.php');
 $db = new Database($config['database']);
 
 // Make database connection available globally
@@ -50,66 +51,80 @@ file_put_contents(
 	FILE_APPEND
 );
 
-$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$router = new Router();
 
-if ($requestUri == '/doc/upload') {
-	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		$route = 'upload_post';
-	} else {
-		$route = 'upload';
-	}
-} else {
-	// Original route determination for backward compatibility
-	$route = isset($_GET['route']) ? $_GET['route'] : 'list';
-}
+$routes = require base_path('routes.php');
 
-switch ($route) {
-	case 'list':
-		require_once base_path('controllers/DocumentController.php');
-		$controller = new DocumentController($db);
-		$controller->listDocuments();
-		break;
-	case 'upload':
-		require_once base_path('controllers/DocumentController.php');
-		$controller = new DocumentController($db);
-		$controller->showUploadForm();
-		break;
-	case 'upload_post':
-		require_once base_path('controllers/DocumentController.php');
-		$controller = new DocumentController($db);
-		$controller->uploadDocument();
-		break;
-	case 'view':
-		require_once base_path('controllers/DocumentController.php');
-		$controller = new DocumentController($db);
-		$controller->viewDocument();
-		break;
-	case 'download':
-		require_once base_path('controllers/DocumentController.php');
-		$controller = new DocumentController($db);
-		$controller->downloadDocument();
-		break;
-	case 'delete':
-		require_once base_path('controllers/DocumentController.php');
-		$controller = new DocumentController($db);
-		$controller->deleteDocument();
-		break;
-	case 'add_category':
-		require_once base_path('controllers/DocumentController.php');
-		$controller = new DocumentController($db);
-		$controller->addCategory();
-		break;
-	case 'delete_category':
-		require_once base_path('controllers/DocumentController.php');
-		$controller = new DocumentController($db);
-		$controller->deleteCategory();
-		break;
-	case 'get_category_count':
-		require_once base_path('controllers/DocumentController.php');
-		$controller = new DocumentController($db);
-		$controller->getCategoryDocumentCount();
-		break;
-	default:
-		view('404.view.php', ['pageTitle' => '404 - Page Not Found']);
-		break;
-}
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
+ 
+// $controller = new DocumentController($db);
+// $router->get('/', $controller->listDocuments());
+
+// $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// if ($requestUri == '/doc/upload') {
+// 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// 		$route = 'upload_post';
+// 	} else {
+// 		$route = 'upload';
+// 	}
+// } else {
+// 	// Original route determination for backward compatibility
+// 	$route = isset($_GET['route']) ? $_GET['route'] : 'list';
+// }
+
+
+// switch ($route) {
+// 	case 'list':
+// 		require_once base_path('controllers/DocumentController.php');
+// 		$controller = new DocumentController($db);
+// 		$controller->listDocuments();
+// 		break;
+// 	case 'upload':
+// 		require_once base_path('controllers/DocumentController.php');
+// 		$controller = new DocumentController($db);
+// 		$controller->showUploadForm();
+// 		break;
+// 	case 'upload_post':
+// 		require_once base_path('controllers/DocumentController.php');
+// 		$controller = new DocumentController($db);
+// 		$controller->uploadDocument();
+// 		break;
+// 	case 'view':
+// 		require_once base_path('controllers/DocumentController.php');
+// 		$controller = new DocumentController($db);
+// 		$controller->viewDocument();
+// 		break;
+// 	case 'download':
+// 		require_once base_path('controllers/DocumentController.php');
+// 		$controller = new DocumentController($db);
+// 		$controller->downloadDocument();
+// 		break;
+// 	case 'delete':
+// 		require_once base_path('controllers/DocumentController.php');
+// 		$controller = new DocumentController($db);
+// 		$controller->deleteDocument();
+// 		break;
+// 	case 'add_category':
+// 		require_once base_path('controllers/DocumentController.php');
+// 		$controller = new DocumentController($db);
+// 		$controller->addCategory();
+// 		break;
+// 	case 'delete_category':
+// 		require_once base_path('controllers/DocumentController.php');
+// 		$controller = new DocumentController($db);
+// 		$controller->deleteCategory();
+// 		break;
+// 	case 'get_category_count':
+// 		require_once base_path('controllers/DocumentController.php');
+// 		$controller = new DocumentController($db);
+// 		$controller->getCategoryDocumentCount();
+// 		break;
+// 	default:
+// 		view('404.view.php', ['pageTitle' => '404 - Page Not Found']);
+// 		break;
+// }
---
+++ htdocs/routes.php
@@ -0,0 +1,4 @@
+<?php
+
+$router->get('/', 'controllers/documents/index.php');
+// $router->get('/notes', 'notes/index.php')->only('auth');
